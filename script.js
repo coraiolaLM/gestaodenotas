838ba193d19e539848d0fb128fa2086b83bc3b9d
Bruno Coraiola <142634278+coraiolaLM@users.noreply.github.com>
2024-04-26 14:33:36 -0300

    script.js
    v0.4.2 - degudar função de edião

--- script.js
+++ script.js
@@ -70,7 +70,72 @@ const addDadosAluno = () => {
 };
 
 const editarAluno = (button) => {
-    console.log("Editar aluno");
+         const row = button.parentNode.parentNode;
+         const cells = row.getElementsByTagName('td');
+    
+         const nome = cells[0].innerText;
+         const ra = cells[1].innerText;
+         const email = cells[2].innerText;
+    
+         const prova1 = parseFloat(localStorage.getItem(ra + '_prova1'));
+         const aep1 = parseFloat(localStorage.getItem(ra + '_aep1'));
+         const provaIntegrada1 = parseFloat(localStorage.getItem(ra + '_provaIntegrada1'));
+         const prova2 = parseFloat(localStorage.getItem(ra + '_prova2'));
+         const aep2 = parseFloat(localStorage.getItem(ra + '_aep2'));
+         const provaIntegrada2 = parseFloat(localStorage.getItem(ra + '_provaIntegrada2'));
+    
+         document.getElementById('input_nome').value = nome;
+         document.getElementById('input_ra').value = ra;
+         document.getElementById('input_email').value = email;
+         document.getElementById('input_prova_1').value = prova1;
+         document.getElementById('input_prova_2').value = prova2;
+         document.getElementById('input_aep_1').value = aep1;
+         document.getElementById('input_aep_2').value = aep2;
+         document.getElementById('input_prova_integrada_1').value = provaIntegrada1;
+         document.getElementById('input_prova_integrada_2').value = provaIntegrada2;
+    
+    
+         const formButton = document.getElementById('formCadastro').querySelector('button');
+         formButton.innerText = 'Atualizar Aluno';
+         formButton.onclick = () => {
+             const novaProva1 = parseFloat(document.getElementById('input_prova_1').value);
+             const novaAep1 = parseFloat(document.getElementById('input_aep_1').value);
+             const novaProvaIntegrada1 = parseFloat(document.getElementById('input_prova_integrada_1').value);
+             const novaProva2 = parseFloat(document.getElementById('input_prova_2').value);
+             const novaAep2 = parseFloat(document.getElementById('input_aep_2').value);
+             const novaProvaIntegrada2 = parseFloat(document.getElementById('input_prova_integrada_2').value);
+             const novaMediaBimestre1 = Math.min(10, novaProva1 + novaAep1 + novaProvaIntegrada1);
+             const novaMediaBimestre2 = Math.min(10, novaProva2 + novaAep2 + novaProvaIntegrada2);
+             const novaMediaFinal = (novaMediaBimestre1 + novaMediaBimestre2) / 2;
+    
+             let novoStatus;
+             if (novaMediaFinal >= 6) {
+                        novoStatus = "Aprovado";
+             } else if (novaMediaFinal >= 3) {
+                        novoStatus = "Recuperação";
+             } else {
+                        novoStatus = "Reprovado";
+             }
+             cells[0].innerText = document.getElementById('input_nome').value;
+             cells[1].innerText = document.getElementById('input_ra').value;
+             cells[2].innerText = document.getElementById('input_email').value;
+             cells[3].innerText = novaMediaBimestre1.toFixed(2);
+             cells[4].innerText = novaMediaBimestre2.toFixed(2);
+             cells[5].innerText = novaMediaFinal.toFixed(2);
+             cells[6].innerText = novoStatus;
+    
+             formButton.innerText = 'Cadastrar Aluno';
+             formButton.onclick = addDadosAluno;
+    
+         localStorage.setItem(ra + '_prova1', novaProva1.toString());
+         localStorage.setItem(ra + '_aep1', novaAep1.toString());
+         localStorage.setItem(ra + '_provaIntegrada1', novaProvaIntegrada1.toString());
+         localStorage.setItem(ra + '_prova2', novaProva2.toString());
+         localStorage.setItem(ra + '_aep2', novaAep2.toString());
+         localStorage.setItem(ra + '_provaIntegrada2', novaProvaIntegrada2.toString());
+    
+         document.getElementById('formCadastro').reset();
+         };
 };
 
 const excluirAluno = (button) => {
